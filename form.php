fd011f2e6a02184053211db59db937467d63ec7b
Kiruthik Kumar <122508631+Kiruthikk07@users.noreply.github.com>
2023-08-19 20:45:37 +0530

    Update form.php

--- form.php
+++ form.php
@@ -5,6 +5,7 @@ $email = $_POST['email'];
 $subject = $_POST['subject'];
 $msg = $_POST['msg'];
 $con = mysqli_connect("localhost", "root", "", "form");
+// Query for inserting data into table
 $sql = "INSERT INTO contact_form (Name,Phone_Number,Email,Subject,Message) VALUES ('$name','$pno','$email','$subject','$msg')";
 if (mysqli_query($con, $sql)) {
     echo "Information successfully added!";
